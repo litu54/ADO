3c1b16ccff20dcc2ec038a864f73176bd50d7950
litu54 <rout.kmr@gmail.com>
2026-08-13 00:50:17 +0530

    code mofiy

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,9 +1,19 @@
-module "resource_group" {
-  source = "./../../modules/azurerm_resource_group"
-  rgk    = var.rgk
-}
+# module "resource_group" {
+#   source = "./../../modules/azurerm_resource_group"
+#   rgk    = var.rgk
+# }
+
+# module "storage_account" {
+#   source = "./../../modules/azurerm_storage_account"
+#   stg    = var.sa
+# }
+
 
 module "storage_account" {
-  source = "./../../modules/azurerm_storage_account"
-  stg    = var.sa
+  source = "../../modules/storage_account"
+
+  # your existing variables
+  # ...
+
+  depends_on = [module.resource_group]
 }
