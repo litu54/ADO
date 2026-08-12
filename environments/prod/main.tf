5c0b021fdcfcc320baf5caa4b7b77c8e94d758ee
litu54 <rout.kmr@gmail.com>
2026-08-13 01:17:31 +0530

    add code2

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -3,16 +3,9 @@ module "resource_group" {
   rgk    = var.rgk
 }
 
-# module "storage_account" {
-#   source = "./../../modules/azurerm_storage_account"
-#   stg    = var.sa
-# }
-
 module "storage_account" {
   source = "./../../modules/storage_account"
-   stg    = var.sa
-  # your existing variables
-  # ...
+  stg    = var.sa
 
   depends_on = [module.resource_group]
 }
